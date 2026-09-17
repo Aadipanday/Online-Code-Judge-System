db46ccc92be60077fb95fe88e60273169fdbc955
Aditya Panday <adityapandayji489@gmail.com>
2026-09-17 18:37:58 +0530

    Implement user authentication with JWT and bcrypt; add error handling and response utilities

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,116 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { User } from "../models/user.models.js";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+import cookieParser from "cookie-parser"
+
+const generateAccessAndRefreshToken = async function(userId){
+    const user = await User.findById(userId);
+    const accessToken =  user.generateAccessToken();
+    const refreshToken =  user.generateRefreshToken();
+
+    user.refreshToken = refreshToken
+    await user.save({ validateBeforeSave: false })
+
+    return {accessToken,refreshToken}
+
+}
+
+
+const registerContoller = asyncHandler(async (req, res) => {
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password
+    })
+
+    const createdUser = await User.findById(user?._id).select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201)
+        .json(new ApiResponse(201, createdUser, "user created successfully!!"))
+})
+
+const loginController = asyncHandler(async(req,res) => {
+    // get username or email and password from frontend
+    // validate it 
+    // find the user in db
+    // verify the password
+    // generate refreshtoken and accesstoken 
+    // set cookies
+    // return res
+
+    const { username, email, password } = req.body;
+
+    if(!(username || email) || !password){
+         throw new ApiError(400, "Username or email and password are required");
+    }
+
+    const user = await User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+   if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {accessToken , refreshToken} = await generateAccessAndRefreshToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+            .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
+})
+
+export {
+    registerContoller
+}
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,10 +1,12 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username: {
         type: String,
         required: [true, "username is required"],
-        unique:true
+        unique: true
     },
     email: {
         type: String,
@@ -15,10 +17,13 @@ const userSchema = new Schema({
         type: String,
         required: [true, "password is required"]
     },
+    refreshToken: {
+        type: String
+    },
     role: {
         type: String,
         enum: ["admin", "user"],
-        default : "user"
+        default: "user"
     },
     solvedProblem: [
         {
@@ -33,5 +38,51 @@ const userSchema = new Schema({
         timestamps: true
     }
 )
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.method.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+userSchema.method.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+
+        },
+
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+
 
 export const User = mongoose.model("User", userSchema);
